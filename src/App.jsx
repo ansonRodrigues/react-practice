ca719b3b79b66ac4a08cbb2cd6dc75f7c2966f21
ansonRodrigues <ansonrodrigues18@gmail.com>
2025-11-18 19:44:02 +0530

    Ccreated a folder to store data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import Card from './components/Card'
-import Navbar from './components/Navbar'
-import topShoes from './data/topshoes'
+import React from "react";
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+import topShoes from "./data/topshoes";
 
 const App = () => {
-
   return (
     <>
       <Navbar userName="Anson" age2={23} />
-      <div className='product-card-container'> {topShoes.map((elem)=>{
-        return <Card productImage = {elem.productImage} brandLogo = {elem.brandLogo} prodTitle = {elem.title} 
-        prodDesc = {elem.description} price = {"$ " + elem.price}/>
-
-      })}</div>
+      <div className="product-card-container">
+        {topShoes.map((elem) => {
+          return (
+            <Card
+              productImage={elem.productImage}
+              brandLogo={elem.brandLogo}
+              prodTitle={elem.title}
+              prodDesc={elem.description}
+              price={"$ " + elem.price}
+            />
+          );
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Card = (props) => {
   return (
-    <div className='product-card-container'>
+    <div className="product-card-container">
       <div className="product-card">
-
         {/* Image Section */}
         <div className="product-image">
-          <img
-            src={props.productImage}
-            alt="Nike NK Court Vision Shoes"
-          />
+          <img src={props.productImage} alt="Nike NK Court Vision Shoes" />
 
-          <img
-            src={props.brandLogo}
-            alt="Nike Logo"
-            className="brand-logo"
-          />
+          <img src={props.brandLogo} alt="Nike Logo" className="brand-logo" />
         </div>
 
         {/* Product Info */}
         <div className="product-details">
           <h2 className="product-title">{props.title}</h2>
-          <p className="product-description">
-            {props.prodDesc}
-          </p>
+          <p className="product-description">{props.prodDesc}</p>
         </div>
 
         {/* Footer */}
@@ -34,10 +24,9 @@ const Card = (props) => {
             Buy Now <span className="arrow">↗</span>
           </button>
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
