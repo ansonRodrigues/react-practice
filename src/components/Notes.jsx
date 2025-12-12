0c4440f6edf5a3e0b8ac39e05765480c83629dfa
ansonRodrigues <ansonrodrigues18@gmail.com>
2025-12-12 05:55:17 +0530

    Adjustments in Notes file

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 
 const Notes = () => {
-    const [heading, setHeading] = useState('')
-    const [headingStored, setHeadingStored] = useState('')
+  const [heading, setHeading] = useState("");
+  const [headingStored, setHeadingStored] = useState("");
+  const [desc, setDesc] = useState("");
+  const [descStored, setDescStored] = useState("");
   const submitHandler = (e) => {
     e.preventDefault();
     console.log("Submitted");
-    console.log(heading)
-    setHeadingStored(heading)
+    console.log(heading);
+    setHeadingStored(heading);
+    setDescStored(desc);
   };
   return (
     <div>
@@ -16,10 +19,27 @@ const Notes = () => {
           submitHandler(e);
         }}
       >
-        <input value={heading} onChange={(e)=> {setHeading(e.target.value)}} id="formInput" type="text" placeholder="Enter your title here" />
+        <input
+          value={heading}
+          onChange={(e) => {
+            setHeading(e.target.value);
+          }}
+          id="formInput"
+          type="text"
+          placeholder="Enter your title here"
+        />
+        <input
+          value={desc}
+          onChange={(e) => {
+            setDesc(e.target.value);
+          }}
+          type="text"
+          placeholder="Enter your description here"
+        />
         <button id="formBtn">Submit</button>
       </form>
       <h1>Heading stored is {headingStored}</h1>
+      <h3>Description is {descStored}</h3>
     </div>
   );
 };
