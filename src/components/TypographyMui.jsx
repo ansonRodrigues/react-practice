232457dacbd30981c7e2259daf616c3df7cb8ed6
ansonRodrigues <ansonrodrigues18@gmail.com>
2026-01-18 19:05:52 +0530

    Install Material UI

---
+++ src/components/TypographyMui.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+const Typographymui = () => {
+  return (
+    <div>
+      <Typography variant="h1" color="initial">
+        This is it
+      </Typography>
+      <h1>regular</h1>
+    </div>
+  );
+};
+
+export default Typographymui;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
+import { Typography } from "@mui/material";
 import React from "react";
+import Typographymui from "../components/Typographymui";
+import ResponsiveAppBar from "../components/ResponsiveAppBar";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <ResponsiveAppBar />
+      <Typographymui />
+    </div>
+  );
 };
 
 export default Home;
