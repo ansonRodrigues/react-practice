835bd8e9fcef6fc3e3270b13228022436f3f80ef
ansonRodrigues <ansonrodrigues18@gmail.com>
2026-01-31 16:02:21 +0530

    Add Dropdown common component

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,36 @@
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormHelperText,
+} from "@mui/material";
+
+const Dropdown = ({
+  label,
+  value,
+  onChange,
+  options = [],
+  error = false,
+  helperText = "",
+  fullWidth = true,
+  size = "small",
+}) => {
+  return (
+    <FormControl fullWidth={fullWidth} size={size} error={error}>
+      <InputLabel>{label}</InputLabel>
+
+      <Select value={value} label={label} onChange={onChange}>
+        {options.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </Select>
+
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+    </FormControl>
+  );
+};
+
+export default Dropdown;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useState } from "react";
+import Dropdown from "../components/Dropdown";
 
 const About = () => {
+  const [country, setCountry] = useState("");
+
+  const countryOptions = [
+    { value: "IN", label: "India" },
+    { value: "US", label: "USA" },
+    { value: "UK", label: "UK" },
+  ];
+
   return (
     <div>
       <h1>About</h1>
+      <Dropdown
+        label="Country"
+        value={country}
+        //size="medium"
+        fullWidth={false}
+        onChange={(e) => setCountry(e.target.value)}
+        options={countryOptions}
+        helperText="Please select a country"
+      />
     </div>
   );
 };
